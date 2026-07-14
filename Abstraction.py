2537ec3521e363190027469f86e7324c320c39d7
yehoshua <y0587828030@gmail.com>
2026-07-14 15:17:23 +0300

    2. Two Delivery Types

--- Abstraction.py
+++ Abstraction.py
@@ -13,6 +13,24 @@ bike = BikeDelivery()
 bike.deliver(101)
 
 
+#step 2 
+class DeliveryMethod(ABC):
+    @abstractmethod
+    def deliver(self, order_id):
+        pass
+
+class DroneDeliver(DeliveryMethod):
+    def deliver(self, order_id):
+        print(f"Order {order_id} dropped by drone at your door.")
+
+class CarDelivery(DeliveryMethod):
+    def deliver(self, order_id):
+        print(f"Order {order_id} brought to your building by car.")
+
+sending= DroneDeliver()
+sending.deliver(202)
+sending2=CarDelivery()
+sending2.deliver(202)
 
 
 
@@ -22,3 +40,26 @@ bike.deliver(101)
 
 
 
+
+# # step 2
+# class DeliveryMethod(ABC):
+
+#     @abstractmethod
+#     def deliver(self,order_id):
+#         pass
+
+# class DroneDelivery (DeliveryMethod):
+     
+#      def deliver(self,order_id):
+#         print (f"{order_id} dropped by dron at your door")
+
+        
+# class CarDelivery (DeliveryMethod):
+     
+#      def deliver(self,order_id):
+#         print (f"{order_id} brought to your building by car")
+
+# car = CarDelivery()
+# car.deliver(202)
+# drone = DroneDelivery()
+# drone.deliver(202
