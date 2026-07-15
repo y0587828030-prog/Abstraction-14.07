4bc5d5d8e4f32f6f0d1d55e3b4538564bd9ba016
yehoshua <y0587828030@gmail.com>
2026-07-15 14:32:13 +0300

    3. Multiple Items with a Constructor

--- oop basics.py
+++ oop basics.py
@@ -22,3 +22,20 @@ class Customer:
 
 claint = Customer("yehosh", "coffe")
 claint.greet()
+
+##3. Multiple Items with a Constructor
+class MenuItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def describe(self):
+        print(f"Item: {self.name} | Price: ${self.price}")
+
+claint1 = MenuItem("latta", 4.5)
+claint2 = MenuItem("Croissant", 2.0)
+claint3 = MenuItem("Cold Brew", 5.0)
+
+claint1.describe()
+claint2.describe()
+claint3.describe()
