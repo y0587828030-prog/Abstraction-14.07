2320d79c07b722a7c6241fae35877f6d8df7e460
yehoshua <y0587828030@gmail.com>
2026-07-15 21:26:08 +0300

    4. Three Sports, One Parent

--- inheritance2.py
+++ inheritance2.py
@@ -50,3 +50,35 @@ class Cyclist(Athlete):
 
 cyclist = Cyclist("Mike", 30, "Trek")
 cyclist.describe_gear()
+
+
+##4. Three Sports, One Parent
+class Athlete:
+    def __init__(self, name , country):
+        self.name = name
+        self.country = country
+
+    def greet(self):
+        print(f"{self.name} represents {self.country}")
+
+class Swimmer(Athlete):
+    def __init__(self, name, country,stroke_style):
+        super().__init__(name, country)
+        self.stroke_style =stroke_style
+
+class Runner(Athlete):
+    def __init__(self, name, country,best_distance):
+        super().__init__(name, country)
+        self.best_distance = best_distance
+
+class Cyclist(Athlete):
+    def __init__(self, name, country, race_type):
+        super().__init__(name, country)
+        self.race_type =race_type
+
+swimmer = Swimmer("Lior", "Israel", "freestyle")
+swimmer.greet()
+runner = Runner("Avi", "Kenya", "marathon")
+runner.greet()
+cyclist= Cyclist("Jan", "France", "road")
+cyclist.greet()
