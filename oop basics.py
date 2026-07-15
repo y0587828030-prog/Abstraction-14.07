c52573d09e23f752d7b39546637d239f7db3863a
yehoshua <y0587828030@gmail.com>
2026-07-15 14:26:24 +0300

    2. Customer Greeting

--- oop basics.py
+++ oop basics.py
@@ -1,3 +1,5 @@
+##1. Describe a Menu Item
+
 class MenuItem:
     def __init__(self, name, price):
         self.name = str(name)
@@ -8,3 +10,15 @@ class MenuItem:
 
 item = MenuItem("Espresso",3.5)
 item.describe()
+
+## 2. Customer Greeting
+class Customer:
+    def __init__(self, name,favorite_drink ):
+        self.name =name
+        self.favorite_drink = favorite_drink
+
+    def greet(self):
+        print(f"Hi! I am {self.name} and I would like a {self.favorite_drink}.")
+
+claint = Customer("yehosh", "coffe")
+claint.greet()
